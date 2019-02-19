40301ca92c585046ae1efdcd130e0fdaa2d65abe
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-02-19 21:15:11 +0530

    Shifting Side Navigation from js

--- js/openSidenav.js
+++ js/openSidenav.js
@@ -6,10 +6,8 @@ function openNavigation() {
     state = !state;
     if(state) {
         document.getElementById("snav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
     }
     else {
         document.getElementById("snav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-    }   
+    }
 }

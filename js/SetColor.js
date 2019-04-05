f5ef97afc8cfc950f6ab4fd5dc8cd590205be9de
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-05 12:02:15 +0530

    Nothing

--- js/SetColor.js
+++
@@ -1,8 +0,0 @@
-function setColor(color) {
-    let taskWindow = document.getElementById("taskWin");
-    let inputElem = taskWindow.getElementsByTagName("input")[0];
-    let buttonWindow = taskWindow.getElementsByClassName("buttonWindow")[0];
-    inputElem.style.borderBottomColor = color;
-    buttonWindow.style.backgroundColor = color;
-    changeTaskColor(color);
-}

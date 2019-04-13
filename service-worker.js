37c7fcf5b83e4b53f7ea18889ac2c290fbdacf82
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-13 08:15:48 +0530

    Sw

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const cacheName = 'cache-v1';
+const precacheResources = [
+      '/',
+      'index.html',
+      'login.html',
+      'tasks.html',
+      'important.html',
+      'css/header.css',
+      'css/headermq.css',
+      'css/home.css',
+      'css/listname.css',
+      'css/listnamemq.css',
+      'css/page.css',
+      'css/scroll.css',
+      'css/sidenav.css',
+      'css/sidenavmq.css',
+      'css/taskcontainer.css',
+      'css/taskcontainermq.css',
+      'css/taskmanager.css',
+      'css/taskmenu.css',
+      'images/arrow-right-solid.svg',
+      'images/bars-solid-white.svg',
+      'images/bars-solid.svg',
+      'images/check-solid-white.svg'
+];
+
+self.addEventListener('install', event => {
+      console.log('Service worker install event!');
+      event.waitUntil(
+            caches.open(cacheName)
+                  .then(cache => {
+                        return cache.addAll(precacheResources);
+                  })
+      );
+});
+
+self.addEventListener('activate', event => {
+      console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+      console.log('Fetch intercepted for:', event.request.url);
+      event.respondWith(caches.match(event.request)
+            .then(cachedResponse => {
+                  if (cachedResponse) {
+                        return cachedResponse;
+                  }
+                  return fetch(event.request);
+            })
+      );
+});

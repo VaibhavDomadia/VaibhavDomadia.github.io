8ae591058a6e28bc206470f1ffb0b88bd65a75f0
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-18 06:42:06 +0530

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,11 +1,23 @@
-function openmenufortheme() {
-      let menutheme = document.getElementById("thememenu");
-      menutheme.style.display = "inline-block";
-}
-
+/**
+ * This is to show the state of the dropdown.
+ * true if the dropdown in header is open and false when the dropdown is closed.
+ */
 let stateofdropdown = false;
+
+/**
+ * This is to disable the whole screen except the dropdown.
+ * So that when the dropdown is open no actions can be taken until it is not closed through clicking on the screen.
+ */
 let disable = document.getElementById('disable');
 
+
+/**
+ * This function is used to toggle between opening and closing of dropdown.
+ * When the user opens the dropdown, this function will add class show (css class) to dropdown-content which will show the dropdown.
+ * And on closing the dropdown it will remove the show class (css class) to make it's display none;
+ *
+ * This function will also change the property of disable(global - variable) (already mentioned above) to disable activities on screen.
+ */
 function opendropdownheader() {
       document.getElementById('dropdown-content').classList.toggle("show");
       if(stateofdropdown) {
@@ -16,8 +28,11 @@ function opendropdownheader() {
       }
 }
 
+/**
+ * Handling onclick event, for opening and closing of dropdown menu.
+ */
 window.onclick = function(event) {
-      if(event.target.matches('#head-dropdown-icon-bound')) {
+      if(event.target.matches('#head-dropdown-icon-bound') || event.target.matches('#head-dropdown-icon')) {
             stateofdropdown = !stateofdropdown;
             opendropdownheader();
       }

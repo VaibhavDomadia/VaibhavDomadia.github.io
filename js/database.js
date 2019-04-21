93da4f2ca3f173ce00b95fced87c97d2308f2ee3
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-21 20:41:42 +0530

    c

--- js/database.js
+++ js/database.js
@@ -9,14 +9,4 @@
             messagingSenderId: "364465222352"
       };
       firebase.initializeApp(config);
-
-      var messaging = firebase.messaging();
-      messaging.usePublicVapidKey("BBUYBs9u2AawQCS-gmhCAmk4sQSZwM4iuzvAo9dYSZo4jXZHagEtV9jEUTeEBMuFzhxhMiuEu_SF_9IoJskP_mc");
-      messaging.requestPermission().then(function () {
-            console.log('Notification permission granted.');
-            // TODO(developer): Retrieve an Instance ID token for use with FCM.
-            // ...
-      }).catch(function (err) {
-            console.log('Unable to get permission to notify.', err);
-      });
 }());

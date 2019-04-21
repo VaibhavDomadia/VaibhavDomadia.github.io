1f3b104489c65e92db8aa32223af2fe1f0dc2b69
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-21 20:36:54 +0530

    Update database.js

--- js/database.js
+++ js/database.js
@@ -9,29 +9,14 @@
             messagingSenderId: "364465222352"
       };
       firebase.initializeApp(config);
+
+      var messaging = firebase.messaging();
+      messaging.usePublicVapidKey("BBUYBs9u2AawQCS-gmhCAmk4sQSZwM4iuzvAo9dYSZo4jXZHagEtV9jEUTeEBMuFzhxhMiuEu_SF_9IoJskP_mc");
+      messaging.requestPermission().then(function () {
+            console.log('Notification permission granted.');
+            // TODO(developer): Retrieve an Instance ID token for use with FCM.
+            // ...
+      }).catch(function (err) {
+            console.log('Unable to get permission to notify.', err);
+      });
 }());
-
-const messaging = firebase.messaging();
-messaging.usePublicVapidKey("BBUYBs9u2AawQCS-gmhCAmk4sQSZwM4iuzvAo9dYSZo4jXZHagEtV9jEUTeEBMuFzhxhMiuEu_SF_9IoJskP_mc");
-messaging.requestPermission().then(function () {
-      console.log('Notification permission granted.');
-      // TODO(developer): Retrieve an Instance ID token for use with FCM.
-      // ...
-}).catch(function (err) {
-      console.log('Unable to get permission to notify.', err);
-});
-
-
-
-
-// messaging.requestPermission()
-// .then(function() {
-//       console.log("Got a permission");
-//       return messaging.getToken();
-// })
-// .then(function(token) {
-//       console.log(token);
-// })
-// .catch(function(err) {
-//       console.log("Sorry");
-// })

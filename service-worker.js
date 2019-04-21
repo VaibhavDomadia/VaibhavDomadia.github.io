88120fdbd0c62311e8771050d1064d1c7541f915
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-21 19:43:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -76,3 +76,19 @@ self.addEventListener('fetch', event => {
             })
       );
 });
+
+importScripts('https://www.gstatic.com/firebasejs/5.9.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.9.2/firebase-messaging.js');
+
+// Initialize Firebase
+var config = {
+      apiKey: "AIzaSyAs7J4hlBXsgyfDB--BqVYzHj9fjQYWls8",
+      authDomain: "to-do-dba8b.firebaseapp.com",
+      databaseURL: "https://to-do-dba8b.firebaseio.com",
+      projectId: "to-do-dba8b",
+      storageBucket: "to-do-dba8b.appspot.com",
+      messagingSenderId: "364465222352"
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();

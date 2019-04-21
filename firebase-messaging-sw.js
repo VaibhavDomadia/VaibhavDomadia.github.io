a0b3dff22a6a4df2cef10157a7adeff9c59bc796
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-21 22:52:59 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,16 +3,6 @@
 //       importScripts("https://www.gstatic.com/firebasejs/5.9.2/firebase-messaging.js");
 // }
 
-const config = {
-      apiKey: "AIzaSyAs7J4hlBXsgyfDB--BqVYzHj9fjQYWls8",
-      authDomain: "to-do-dba8b.firebaseapp.com",
-      databaseURL: "https://to-do-dba8b.firebaseio.com",
-      projectId: "to-do-dba8b",
-      storageBucket: "to-do-dba8b.appspot.com",
-      messagingSenderId: "364465222352"
-};
-firebase.initializeApp(config);
-
 // Get Instance ID token. Initially this makes a network call, once retrieved
 // subsequent calls to getToken will return from cache.
 messaging.getToken().then(function (currentToken) {

9683b8e19961f89dba161c428d6859d6a7fe9240
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-04-21 20:23:05 +0530

    c

--- js/database.js
+++ js/database.js
@@ -1,25 +1,37 @@
 (function initdatabase() {
       // Initialize Firebase
       var config = {
-      apiKey: "AIzaSyAs7J4hlBXsgyfDB--BqVYzHj9fjQYWls8",
-      authDomain: "to-do-dba8b.firebaseapp.com",
-      databaseURL: "https://to-do-dba8b.firebaseio.com",
-      projectId: "to-do-dba8b",
-      storageBucket: "to-do-dba8b.appspot.com",
-      messagingSenderId: "364465222352"
+            apiKey: "AIzaSyAs7J4hlBXsgyfDB--BqVYzHj9fjQYWls8",
+            authDomain: "to-do-dba8b.firebaseapp.com",
+            databaseURL: "https://to-do-dba8b.firebaseio.com",
+            projectId: "to-do-dba8b",
+            storageBucket: "to-do-dba8b.appspot.com",
+            messagingSenderId: "364465222352"
       };
       firebase.initializeApp(config);
 }());
 
 const messaging = firebase.messaging();
-messaging.requestPermission()
-.then(function() {
-      console.log("Got a permission");
-      return messaging.getToken();
-})
-.then(function(token) {
-      console.log(token);
-})
-.catch(function(err) {
-      console.log("Sorry");
-})
+messaging.usePublicVapidKey("BBUYBs9u2AawQCS-gmhCAmk4sQSZwM4iuzvAo9dYSZo4jXZHagEtV9jEUTeEBMuFzhxhMiuEu_SF_9IoJskP_mc");
+messaging.requestPermission().then(function () {
+      console.log('Notification permission granted.');
+      // TODO(developer): Retrieve an Instance ID token for use with FCM.
+      // ...
+}).catch(function (err) {
+      console.log('Unable to get permission to notify.', err);
+});
+
+
+
+
+// messaging.requestPermission()
+// .then(function() {
+//       console.log("Got a permission");
+//       return messaging.getToken();
+// })
+// .then(function(token) {
+//       console.log(token);
+// })
+// .catch(function(err) {
+//       console.log("Sorry");
+// })
